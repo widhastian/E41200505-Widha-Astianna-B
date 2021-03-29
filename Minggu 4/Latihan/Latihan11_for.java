c429d20f66433bff86f3354551f1ab094e0d0a87
Widha Astianna <widhaastianna10@gmail.com>
2021-03-29 18:33:27 +0700

    Latihan

---
+++ Minggu 4/Latihan/Latihan11_for.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanWSIBD;
+
+/**
+ *
+ * @author hp
+ */
+public class Latihan11_for {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int a = 0; a <= 5; a++) {
+            for(int b = 0; b <= a; b++) {
+                System.out.print("+");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
---
+++ Minggu 4/Latihan/Latihan12_for_each.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanWSIBD;
+
+/**
+ *
+ * @author hp
+ */
+public class Latihan12_for_each {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numbers[] = {1,2,3,4,5};
+        for (int x : numbers) {
+            System.out.print(x + " ");
+        }
+    }
+    
+}
