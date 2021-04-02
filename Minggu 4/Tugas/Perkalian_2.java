0e11d299e48dc9b360a6b2ad98b0389cbe1ee88b
Widha Astianna <widhaastianna10@gmail.com>
2021-04-02 08:57:27 +0700

    Tugas

--- Minggu 4/Tugas/Perkalian_2.java
+++ Minggu 4/Tugas/Perkalian_2.java
@@ -9,21 +9,20 @@ package TugasWSIBD.Minggu4;
  *
  * @author hp
  */
-public class Kelipatan_2 {
+public class Perkalian_2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("         Do_While         ");
+        System.out.println("==========================");
+        System.out.println("    Perkalian 2 (1-100)   ");
         
-        System.out.println("          Do_While          ");
-        System.out.println("Bilangan Kelipatan 2 (1-100)");
-        System.out.println("============================");
-        
-        for(int a=2; a<=100; a+=2) {
-            System.out.print(a + " ");
+        for (int i=2; i<=100; i*=2) {
+            System.out.print(i + " ");
         }
-    } 
+    }
     
 }
